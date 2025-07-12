a2ce9dfaf29026ac22c7d21085076ed279def02e
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-07-12 21:26:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+	return new Promist((resolve, reject)=>{
+		const img = new Image();
+		img.src = url;
+		img.onload = () => resolve(img);
+		img.onerror = () => reject(new Error(`Failed to load image: ${url}`));
+	});
+}
+function downloadImages(){
+	error.textContent = '';
+	output.innerHTML = '';
+
+	loading.style.display = "block";
+
+	Promise.all(images.map(img =>downloadImage(img.url)))
+	.then(downloadImages => {
+		loading.style.display = "none";
+		loadedImages.forEach(img => output.appendChild(img));
+	})
+	.catch(err =>{
+		loading.style.display = "none";
+		error.textContent = err.message;
+	});
+}
+btn.addEventListener("click" downloadImages);
